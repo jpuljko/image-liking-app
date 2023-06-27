20784b47e340cd0d8188b1ccffb7746fc5875ebe
jpuljko <jxp5761@rit.edu>
2023-06-27 19:18:53 +0200

    Connecting the heart icon with the number of likes, adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { Container, MantineProvider } from "@mantine/core";
 import LikePhotoApp from "./LikePhotoApp";
 
 function App() {
   return (
-    <LikePhotoApp />
+    <Container>
+          <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'dark',
+        colors: {
+          // override dark colors to change them for all components
+          dark: [
+            '#d5d7e0',
+            '#acaebf',
+            '#8c8fa3',
+            '#666980',
+            '#4d4f66',
+            '#34354a',
+            '#2b2c3d',
+            '#1d1e30',
+            '#0c0d21',
+            '#01010a',
+          ],
+        },
+      }}
+    >
+      <LikePhotoApp />
+    </MantineProvider>
+      
+    </Container>
+    
   );
 }
 
--- src/LikePhotoApp.jsx
+++ src/LikePhotoApp.jsx
@@ -1,10 +1,24 @@
 import { Container, Space, Title, Text, Card, Image, Grid  } from "@mantine/core"
 import { AiOutlineHeart, AiFillHeart, AiOutlineComment } from "react-icons/ai"
 import bowserJr from "./img/bowserJr.png"
+import { useState } from "react";
 
 export default function LikePhotoApp() {
     
-    let like = false;
+    // let like = false;
+    const [like, setLike] = useState(false);
+    const [count, setCount] = useState(0);
+
+    const toggleLike = () => {
+        if (!like) {
+            setLike(true);
+            setCount(count + 1);
+        } else {
+            setLike(false);
+            setCount(count - 1);
+        }
+    }
+
 
     return (
     <Container size="20rem" px={0}>
@@ -16,7 +30,7 @@ export default function LikePhotoApp() {
 
         <Space h="xl" />
 
-        <Title size="h4" align="center">Likes</Title>
+        <Title size="h4" align="center">Likes {count}</Title>
 
         <Space h="xl" />
 
@@ -24,7 +38,6 @@ export default function LikePhotoApp() {
             shadow="sm"
             padding="xl"
             component="a"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
             target="_blank"
         >
             <Text weight={500} fz="xl" mt="md" align="center" fw={700}>
@@ -33,18 +46,18 @@ export default function LikePhotoApp() {
 
             <Space h="xl" />
 
-            <Card.Section height={400}>
+            <Card.Section height={400} color="red">
                 <Image
                 src={bowserJr}
-                height={300}
+                height={500}
                 alt="Bowser Jr jumping"
                 />
             </Card.Section>
 
             <Card.Section inheritPadding mt="sm" pb="md">
                 <Grid>
-                    <Grid.Col span="auto"><AiOutlineComment /> </Grid.Col>
-                    <Grid.Col span={2}>{like ? (<AiFillHeart />) : (<AiOutlineHeart />)}</Grid.Col>
+                    <Grid.Col span="auto" fz="xl"><AiOutlineComment /> </Grid.Col>
+                    <Grid.Col span={2} fz="xl">{like ? (<AiFillHeart color="red" onClick={toggleLike} />) : (<AiOutlineHeart onClick={toggleLike} />)}</Grid.Col>
                 </Grid>
             </Card.Section>
         </Card>
